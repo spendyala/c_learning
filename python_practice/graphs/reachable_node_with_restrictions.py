e8c33ffa4307ed6085b6a5b4ff56557fd7a59faf
pariveshpriye <parivesh.priye@gmail.com>
2024-03-18 08:26:10 -0700

    Solution to reachable nodes from 0 if we have restricted nodes

---
+++ python_practice/graphs/reachable_node_with_restrictions.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+
+        restricted_set = set(restricted)
+
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        seen = set()
+        stack = []
+
+        seen.add(0)
+        stack.append(0)
+        reached = 1
+
+        while stack:
+            node = stack.pop()
+
+            neighbors = graph[node]
+
+            for neighbor in neighbors:
+                if neighbor in restricted_set:
+                    continue
+                if neighbor not in seen:
+                    reached += 1
+                    seen.add(neighbor)
+                    stack.append(neighbor)
+        return reached
