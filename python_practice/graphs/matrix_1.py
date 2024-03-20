2e86fee20f99197d063430d6d6b0bf6de06407b7
pariveshpriye <parivesh.priye@gmail.com>
2024-03-20 08:43:39 -0700

    in place update

--- python_practice/graphs/matrix_1.py
+++ python_practice/graphs/matrix_1.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/01-matrix/
+from typing import List
+from collections import deque
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
@@ -33,3 +35,29 @@ class Solution:
                         queue.append((each_x, each_y))
             distance += 1
         return output
+
+    def answer(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        queue = deque()
+
+        # Do it in place.
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    queue.append((i, j))
+                else:
+                    mat[i][j] = float('inf')
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while queue:
+            x, y = queue.popleft()
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+
+                # Update distance if distance + 1 is less than previous
+                if 0 <= nx < m and 0 <= ny < n and mat[nx][ny] > mat[x][y] + 1:
+                    mat[nx][ny] = mat[x][y] + 1
+                    queue.append((nx, ny))
+
+        return mat
